ef38ac9aeba251f45c1932ae0f58a635f4d1ee1e
Bradley Justice <bjustice@buxtonco.com>
2023-09-13 21:31:07 -0500

    Update tests

--- dropout-dl-ui.Tests/DownloadTests.cs
+++ dropout-dl-ui.Tests/DownloadTests.cs
@@ -5,16 +5,32 @@ namespace dropout_dl_ui.Tests
 {
     public class DownloadTests
     {
-        [Fact]
-        public void TestUrlsParseToCorrectContentType()
+        [Theory]
+        [InlineData("https://www.dropout.tv/dirty-laundry/season:1/videos/who-stayed-up-for-81-hours")]
+        [InlineData("https://www.dropout.tv/dimension-20/season:1/videos/episode-1")]
+        [InlineData("https://www.dropout.tv/make-some-noise/season:1/videos/hbo-s-a-game-of-rock-paper-scissors")]
+        public void TestEpisodeUrlParsesToCorrectContentType(string url)
         {
-            var episodeUrl = "https://www.dropout.tv/make-some-noise/season:1/videos/hbo-s-a-game-of-rock-paper-scissors";
-            var seasonUrl = "https://www.dropout.tv/make-some-noise/season:1";
-            var seriesUrl = "https://www.dropout.tv/make-some-noise";
-
-            Assert.Equal(DropoutContentType.Episode, DownloadLogic.GetContentTypeFromUrl(episodeUrl));
-            Assert.Equal(DropoutContentType.Season, DownloadLogic.GetContentTypeFromUrl(seasonUrl));
-            Assert.Equal(DropoutContentType.Series, DownloadLogic.GetContentTypeFromUrl(seriesUrl));
+            var actualContentType = DownloadLogic.GetContentTypeFromUrl(url);
+            Assert.Equal(DropoutContentType.Episode, actualContentType);
+        }
+        [Theory]
+        [InlineData("https://www.dropout.tv/dirty-laundry/season:1")]
+        [InlineData("https://www.dropout.tv/dimension-20/season:5")]
+        [InlineData("https://www.dropout.tv/make-some-noise/season:2")]
+        public void TestSeasonUrlParsesToCorrectContentType(string url)
+        {
+            var actualContentType = DownloadLogic.GetContentTypeFromUrl(url);
+            Assert.Equal(DropoutContentType.Season, actualContentType);
+        }
+        [Theory]
+        [InlineData("https://www.dropout.tv/dirty-laundry")]
+        [InlineData("https://www.dropout.tv/dimension-20")]
+        [InlineData("https://www.dropout.tv/make-some-noise")]
+        public void TestSeriesUrlParsesToCorrectContentType(string url)
+        {
+            var actualContentType = DownloadLogic.GetContentTypeFromUrl(url);
+            Assert.Equal(DropoutContentType.Series, actualContentType);
         }
     }
 }
