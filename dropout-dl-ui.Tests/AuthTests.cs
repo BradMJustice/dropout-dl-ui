6ea505fc8437ac4c53aaf4636fce10e7e310b3b4
Bradley Justice <bjustice@buxtonco.com>
2023-09-14 00:02:17 -0500

    I can now load a page and get HTML

--- dropout-dl-ui.Tests/AuthTests.cs
+++ dropout-dl-ui.Tests/AuthTests.cs
@@ -1,4 +1,5 @@
 ﻿using dropout_dl.Common.Logic;
+using HtmlAgilityPack;
 
 namespace dropout_dl_ui.Tests
 {
@@ -7,7 +8,13 @@ namespace dropout_dl_ui.Tests
         [Fact]
         public async Task TestLoginWithCredentials()
         {
-            await AuthLogic.LoginWithCredentials("BradMJustice@gmail.com", "Fender04Dropout!");
+            await DropoutBrowser.InitializeBrowser();
+            var html = await DropoutBrowser.GetPageHtml("https://www.dropout.tv/login");
+
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var node = document.DocumentNode.SelectSingleNode("//h1");
         }
     }
 }
---
+++ dropout-dl.Common/Logic/DropoutBrowser.cs
@@ -0,0 +1,54 @@
+﻿using PuppeteerSharp;
+using System.Runtime.InteropServices;
+
+namespace dropout_dl.Common.Logic
+{
+    public static class DropoutBrowser
+    {
+        private static string _chromiumPath = "";
+        public static async Task InitializeBrowser()
+        {
+            var browserFetcherOptions = new BrowserFetcherOptions
+            {
+                Path = ChromiumPath()
+            };
+            using var browserFetcher = new BrowserFetcher(browserFetcherOptions);
+            var chromiumRevision = PuppeteerSharp.BrowserData.Chrome.DefaultBuildId;
+            var browser = await browserFetcher.DownloadAsync(chromiumRevision);
+            _chromiumPath = browser.GetExecutablePath();
+        }
+        public static async Task<string> GetPageHtml(string url)
+        {
+            using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                ExecutablePath = _chromiumPath
+            });
+
+            using var page = await browser.NewPageAsync();
+            var navOptions = new NavigationOptions
+            {
+                WaitUntil = new WaitUntilNavigation[]
+                {
+                    WaitUntilNavigation.Networkidle0
+                },
+                Timeout = 10000
+            };
+
+            await page.GoToAsync(url, navOptions);
+
+            var content = await page.GetContentAsync();
+            return content;
+        }
+        private static string ChromiumPath()
+        {
+            return IsLinux()
+                ? "/tmp"
+                : Directory.GetCurrentDirectory();
+        }
+        public static bool IsLinux()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+    }
+}
